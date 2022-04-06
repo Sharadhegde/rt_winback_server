9e1ca805e0e5f6c3791fcd5d02672ecc647fac79
Stuti Biyani <stutibiyani@stutis-mbp.myfiosgateway.com>
2022-04-05 20:03:25 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, make_response, jsonify
+from routes import *
+# from waitress import serve
+# from flask_cors import CORS
+
+app = Flask(__name__)
+app.register_blueprint(routes)
+
+# cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
+    # serve(app, port=5000)
---
+++ routes/__init__.py
@@ -0,0 +1,20 @@
+import json
+from sqlalchemy.engine.url import URL
+from sqlalchemy import create_engine, engine_from_config
+from flask import Blueprint
+
+from .config import config
+
+routes = Blueprint('routes', __name__)
+
+try:
+    """Load the config details for SQL server
+    Create the cursor and connection objects
+    """
+    params = config('./routes/config.ini', 'mysql') 
+    url = URL.create(**params)
+    engine = create_engine(url, echo=True)
+    conn = engine.connect()
+
+except Exception as e:
+    print(str(e))
